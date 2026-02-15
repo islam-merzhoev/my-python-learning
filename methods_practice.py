6621f6bb379d9e6fa20779392bd47db87cfbc89e
islam-merzhoev <vvipsofter@gmail.com>
2026-02-15 18:11:07 +0300

    Create methods_practice.py

---
+++ methods_practice.py
@@ -0,0 +1,5 @@
+raw_nickname = "   iSlAm_51   "
+clean_nickname = raw_nickname.strip().lower().capitalize()
+print(f"Original: '{raw_nickname}'")
+print(f"Cleaned: '{clean_nickname}'")
+print(f"Check original again: '{raw_nickname}' (Still dirty!)")
